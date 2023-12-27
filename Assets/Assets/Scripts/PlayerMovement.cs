369e62a0d30a743f3593c9e375a584686c21d86f
SavoryBanana <patrickmayor.donghil@bicol-u.edu.ph>
2023-12-27 21:40:19 +0800

    Prototype changes: Add player movement script and cinemachine

---
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpeed = 10;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionY = Input.GetAxisRaw("Vertical");
+        
+        playerDirection = new Vector2(directionX, directionY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+    }
+}
